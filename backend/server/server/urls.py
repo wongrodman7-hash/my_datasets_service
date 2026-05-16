9faed4d902ec8a123e5d8a04c46b5bb0b945eb52
Rodman Wong <wongrodman7@gmail.com>
2026-05-16 19:35:22 +0800

    Add ML code, algorithms, and Django endpoints models

---
+++ backend/server/server/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += endpoints_urlpatterns
